da5b7683b4806f916e365a957e373933c9bdcb98
Carlos Darío CDT. Torres <ctorres@ferracioli.local>
2023-01-24 20:47:22 -0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,71 @@
+<script>
+import { text } from "svelte/internal";
+import Interior from "./interior.svelte";
+export let  valor;
+ let nombre="";
+ function gestionarClick(){
+   alert("ok");
+ }
+
+ function chequear(e){
+ const target= e.target;
+nombre= e.target.value;
+ 
+ }
+
+</script>
+
+
+<h1>Svelte</h1>
+<Interior prg="Ventas" idCliente=11 deuda={valor}/>
+
+<hr>
+
+
+ <input type="text" placeholder="ingrese su nombre" class="texto" on:keyup={chequear} name="cli">
+ <button disabled={nombre.length<=0} > ingresar </button>
+ {#if nombre.length<=0}
+  <p>No me has dicho tu nombre</p>
+  {:else}
+   <p>Buen dia  {nombre}</p>
+   <tr></tr>
+   <p class="caracteres">cantidad de caracter es de {nombre.length} </p>
+  {/if}
+
+
+
+<style>
+.boton{
+  font-size: 1rem;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  color:black;
+  background-color: blueviolet;
+}
+
+.texto{
+  font-size: 2rem;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  }
+
+p{
+  font-size: 25px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  color:royalblue;
+  
+}
+h1{
+   font-size: 7rem;
+  font-family: Georgia, 'Times New Roman', Times, serif ;
+
+}  
+
+.caracteres{
+  font-size: 20px;
+  font-family: 'Courier New', Courier, monospace;
+}
+.title{
+  font-size: 1rem;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  color:aquamarine;
+}
+</style>
---
+++ src/interior.svelte
@@ -0,0 +1,49 @@
+<script>
+    export let prg;
+    export let idCliente; 
+    export let deuda;
+    let tarjetas=["master","visa","American"]
+    let ser=""
+    
+ function ver(e){
+ const target= e.target;
+ ser= e.target.value;
+ 
+ }
+</script>
+ 
+<h1>Modulo de {prg} </h1>
+<hr>
+<p>Su codigo de cliente es  {idCliente} y  su deuda es de $ {deuda}</p>
+
+
+ 
+  <input type="search" name="lbl_1" id="1" on:keyup={ver}>
+
+ <p></p>
+ {#if ser=="master"}    
+ <tr>{ser}  en 12 cuotas con  25 % de recargo</tr>
+{/if}
+
+<hr>
+{#each tarjetas as tar}
+    {#if tar=="master"}    
+        <tr>{tar} con  25 de recargo</tr>
+    {/if}
+    {#if tar=="visa"}    
+        <tr>{tar} con  1    5 de recargo</tr>
+    {/if}
+
+{/each}
+<style>
+    h1{
+        color: cadetblue;
+        font-size: 23px;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
+    p{
+        color: chocolate;
+        font-size: 22px;
+        
+    }
+</style>
