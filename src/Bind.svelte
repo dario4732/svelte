1e2eaa68979e294fe1d27db1652fecc3ba86404f
dario4732 <torres.dario@oca.com.ar>
2023-01-25 20:52:44 -0300

    mas cambios

--- src/Bind.svelte
+++ src/Bind.svelte
@@ -1,11 +1,34 @@
 <script>
     let nombre="local" ;
+    let valorU="google.com";
+    let opcion=false;
+    let sel=["lunes","martes","miercoles","jueves","viernes","sabado","domingo"];
+    let sele="lunes";
+
     function ver(e){
- const target= e.target;
- nombre= e.target.value;
- 
- }
+    const target= e.target;
+    nombre= e.target.value;
+    }
 </script>
 
-<p> tu servidor actual es {nombre} </p>
+<p> tu servidor actual es => <strong>{nombre}</strong> </p>
 <p><input type="text" value={nombre} on:keyup = {ver} />  </p>
+<hr>
+<p> tu servidor secundario =><strong>{valorU}</strong>  </p>
+<p><input type="text" bind:value={valorU}></p>
+
+{#if opcion}
+    <p>Si esta de  Acuerdo</p>
+{:else}
+    <p>No esta de Acuerdo</p>
+{/if}
+<input type="checkbox" bind:checked={opcion} value={opcion}>
+
+<hr>
+<select bind:value={sele}>
+    {#each sel as s}
+    <option value={s}>{s}</option>  
+    {/each}
+</select>
+
+<p>Su franco  sera el {sele}</p>
